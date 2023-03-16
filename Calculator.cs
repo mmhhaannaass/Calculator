30de85534a068683a0b2ffeb22d0e4162893e6a5
mmhhaannaass <muhammad.1401@students.amikom.ac.id>
2023-03-16 15:00:21 +0700

    menambah fungsi Pembagian/

--- Calculator.cs
+++ Calculator.cs
@@ -7,6 +7,8 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a,b, Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
+		Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
 		
 		Console.WriteLine("\Tekan sembarang key untuk keluar");
 		Console.ReadKey();
@@ -26,5 +28,9 @@ class Calculator
 	static int Perkalian(int a, intb)
 	{
 		return a * b;
+	}
+	static int Pembagian(int a, b)
+	{
+		return a / b;
 	}
 }
